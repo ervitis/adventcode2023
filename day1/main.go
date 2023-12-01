6731e78460cca1ddb8ad4c8dff79cc05b488d82b
ervitis <victor.martinalonso@hrbrain.co.jp>
2023-12-01 22:23:56 +0900

    day 1 part 1

--- day1/main.go
+++ day1/main.go
@@ -1,1 +1,55 @@
-package day1
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("./day1/input")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	scanner := bufio.NewScanner(f)
+	part1(scanner)
+}
+
+func part1(scanner *bufio.Scanner) {
+	regx, err := regexp.Compile(`\d+`)
+	if err != nil {
+		panic(err)
+	}
+
+	n := 0
+	var i int
+	for scanner.Scan() {
+		data := strings.Join(regx.FindAllString(scanner.Text(), -1), "")
+		if len(data) > 1 {
+			s := fmt.Sprintf("%s%s", string(data[0]), string(data[len(data)-1]))
+			i, err = strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+		} else if len(data) == 1 {
+			s := fmt.Sprintf("%s%s", string(data[0]), string(data[0]))
+			i, err = strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			i = 0
+		}
+		n += i
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println(n)
+}
