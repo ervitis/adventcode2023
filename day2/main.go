8ade9596a41c531de1a9d60d5cc42ebde0c91fbd
ervitis <victor.martinalonso@hrbrain.co.jp>
2023-12-03 21:21:02 +0900

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -12,7 +12,58 @@ const Green = 13
 const Red = 12
 
 func main() {
+	fmt.Println(part2())
 	fmt.Println(part1())
+}
+
+func part2() int {
+	idx := 0
+	sc := common.GetScanner("./day2/input")
+	for sc.Scan() {
+		txt := sc.Text()
+		games := strings.Split(txt, ":")
+		gameData := strings.TrimSpace(games[1])
+		groups := strings.Split(gameData, ";")
+		set := make([]map[string]int, len(groups))
+		for k := range groups {
+			set[k] = map[string]int{
+				"blue":  0,
+				"green": 0,
+				"red":   0,
+			}
+		}
+
+		for i, gr := range groups {
+			gr = strings.TrimSpace(gr)
+			balls := strings.Split(gr, ",")
+			for _, ball := range balls {
+				ball = strings.TrimSpace(ball)
+				num, _ := strconv.Atoi(strings.Split(ball, " ")[0])
+				if strings.Contains(ball, "red") {
+					set[i]["red"] += num
+				}
+				if strings.Contains(ball, "blue") {
+					set[i]["blue"] += num
+				}
+				if strings.Contains(ball, "green") {
+					set[i]["green"] += num
+				}
+			}
+		}
+		maxB := 1
+		maxG := 1
+		maxR := 1
+		for i := range set {
+			maxB = max(maxB, set[i]["blue"])
+			maxG = max(maxG, set[i]["green"])
+			maxR = max(maxR, set[i]["red"])
+		}
+		idx += maxB * maxG * maxR
+	}
+	if err := sc.Err(); err != nil {
+		panic(err)
+	}
+	return idx
 }
 
 func part1() int {
