a1e3c7841617ab04ed05ef00c9b42f6bee864217
ervitis <victor.martinalonso@hrbrain.co.jp>
2023-12-05 23:35:38 +0900

    day 3 part 1

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Day 1: https://adventofcode.com/2023/day/1
 - Day 2: https://adventofcode.com/2023/day/2
+- Day 3: https://adventofcode.com/2023/day/3 
---
+++ python/day3/main.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+data = open('input').read().strip()
+lines = data.split('\n')
+matrix = [[c for c in line] for line in lines]
+N = len(matrix)
+M = len(matrix[0])
+
+p1 = 0
+nums = defaultdict(list)
+for i in range(len(matrix)):
+    gears = set()
+    num = 0
+    c_part = False
+    for j in range(len(matrix[i])+1):
+        if j < M and matrix[i][j].isdigit():
+            num = num*10 + int(matrix[i][j])
+            for rr in [-1, 0, 1]:
+                for cc in [-1, 0, 1]:
+                    if 0 <= i+rr < N and 0<j+cc<M:
+                        ch = matrix[i+rr][j+cc]
+                        if not ch.isdigit() and ch != '.':
+                            c_part = True
+                        if ch == '*':
+                            gears.add((i+rr, j+cc))
+        elif num > 0:
+            for gear in gears:
+                nums[gear].append(num)
+            if c_part:
+                p1 += num
+            num = 0
+            c_part = False
+            gears = set()
+
+print(p1)
