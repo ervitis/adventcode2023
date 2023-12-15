15f78f0335e850e1fed321a28b020cf7e8739208
ervitis <victor.martinalonso@hrbrain.co.jp>
2023-12-15 09:17:48 +0900

    day6 part2

--- python/day6/main.py
+++ python/day6/main.py
@@ -1,6 +1,8 @@
 raw_data = open('input').read().strip()
 parts = [d.split(':')[1].strip().split() for d in raw_data.split('\n')]
 
+parts2 = [int(d.split(':')[1].strip().replace(' ', '')) for d in raw_data.split('\n')]
+
 p1 = 1
 j = 0
 while j < len(parts[0]):
@@ -17,3 +19,17 @@ while j < len(parts[0]):
     j += 1
 
 print(p1)
+
+# TODO: better performance
+p2 = 0
+n = 0
+t, dmax = parts2[0], parts2[1]
+
+for i in range(t-1):
+    tt = t - i
+    d = i * tt
+    if d <= dmax:
+        continue
+    p2 += 1
+
+print(p2)
